4be19404d11399398a43fc12a265088fa378a1b8
Ilya <iii@ya.ru>
2022-07-09 23:13:50 +0500

    num 29

---
+++ Num29/Program.cs
@@ -0,0 +1,17 @@
+﻿//Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран
+
+int[] arr = new int[8];
+
+void Fill_And_Show(int[] a)
+{
+    int l = a.Length;
+
+        for (int i = 0; i < l; i++)
+        {
+            a[i] = new Random().Next(-20, 20);
+            Console.Write($"{a[i]}|");
+        }
+
+}
+
+Fill_And_Show(arr);
